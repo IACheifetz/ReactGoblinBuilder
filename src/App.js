ebe44abe84dd6be6126d5182d77091bc1c9d13c7
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-05-04 13:14:27 -0700

    added preliminary functionality to delete goblin function in app js

--- src/App.js
+++ src/App.js
@@ -37,10 +37,12 @@ function App() {
 
   function handleDeleteGoblin(name) {
     // find the index of the goblin in allGoblins with this name
-
+    const goblinIndex = allGoblins.findIndex(goblin => goblin.name === name);
     // use splice to delete the goblin object at this index
+    allGoblins.splice(goblinIndex, 1);
 
     // update the allGoblins array immutably to this new, smaller array
+    setAllGoblins([...allGoblins]);
   }
 
   function handleFilterGoblins(search) {
